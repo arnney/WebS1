078cb71e9ffa8b9a089c38ae76997389f4c8bf1b
Arnney <thearnni@gmail.com>
2024-04-10 01:33:53 +0300

    small errors

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo, ObjectId
 from flasgger import Swagger
 from bson.errors import InvalidId
 
 
+
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = 'mongodb://mongo:27017/SupplementStore'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://mongo:27017/SupplementStore')
 mongo = PyMongo(app)
 
 
